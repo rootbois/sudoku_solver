34f327fc8e142a5aed98982d93e8dd498babcc12
RootBoi1 <theson.vu@googlemail.com>
2018-06-27 18:19:33 +0200

    Generator Dateien

---
+++ Classes/Generator.cpp
@@ -0,0 +1,7 @@
+// Copyright 2018, Root Bois Studios,
+// Author: The Son Vu,
+
+#include "./Cells.h"
+
+// ________________________________________________________
+
---
+++ Classes/Generator.h
@@ -0,0 +1,10 @@
+// Copyright 2018, Root Bois Studios,
+// Author: The Son Vu,
+
+#ifndef GENERATOR_H_
+#define GENERATOR_H_
+
+#include <gtest/gtest.h>
+
+// ___________________________________________________________
+
